10d959740b4e9bb4f542022457bc49d69d74a571
sarhanasirhan <sarhanasirhan@gmail.com>
2026-07-15 01:26:18 +0300

    Fix TOKEN import

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 import os 
-token = os.getenv("8603445210:AAG_O8ejNjt3FRQ_wL5VRJCmuHyBRCBT0uo")
-manager_code = "324566"
+TOKEN = os.getenv("8603445210:AAG_O8ejNjt3FRQ_wL5VRJCmuHyBRCBT0uo")
+MANAGER_PASSWORD = "324566"
