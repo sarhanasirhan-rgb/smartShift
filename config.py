159c2a1d8420e2f97d82bc314a6d54c9c71be03c
sarhanasirhan <sarhanasirhan@gmail.com>
2026-07-15 01:04:57 +0300

    Secure token and ignore database

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-TOKEN = "8603445210:AAG_O8ejNjt3FRQ_wL5VRJCmuHyBRCBT0uo"
-
-MANAGER_CODE = "324566"
-
-MAX_WEEKLY_REQUESTS = 3
+import os 
+token = os.getenv("8603445210:AAG_O8ejNjt3FRQ_wL5VRJCmuHyBRCBT0uo")
+manager_code = "324566"
